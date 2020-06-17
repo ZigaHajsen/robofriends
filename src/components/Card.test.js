309c3ef90dafd615befa301c9a987e1d47ee88fc
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-06-18 01:43:31 +0200

    Snapshot Testing

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,7 +1,8 @@
-import { shallow, mount, render } from 'enzyme';
+import { shallow } from 'enzyme';
 import React from 'react';
 import Card from './Card';
 
-it('expect to render Card Component', () => {
-  expect(shallow(<Card />).length).toEqual(1);
+it('expect to render Card component', () => {
+  const cardComponent = shallow(<Card />);
+  expect(cardComponent.debug()).toMatchSnapshot();
 });
